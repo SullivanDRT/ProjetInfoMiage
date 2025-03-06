2e635bb1117d763e3e2f00a9c1212b7755bb96dd
Zineb SUKIENNIK <zineb.sukiennik@etu.univ-orleans.fr>
2025-03-06 21:00:32 +0100

    affichage grill

--- src/Grille.js
+++ src/Grille.js
@@ -1,6 +1,62 @@
-export default class TitleMap {
-  constructor() {}
-  draw() {
-    console.log("draw");
+export default class Grille {
+  constructor(tailleGrille) {
+    this.tailleGrille = tailleGrille;
+    this.temperature = new Image();
+    this.temperature.src = "../images/temperature.png";
+
+    this.viscosite = new Image();
+    this.viscosite.src = "../images/viscocite.png";
+
+    this.mur = new Image();
+    this.mur.src = "../images/mur.png";
+
+    this.caseVide = new Image();
+    this.caseVide.src = "../images/case.png";
+  }
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  draw(ctx) {
+    for (let ligne = 0; ligne < this.map.length; ligne++) {
+      for (let colonne = 0; colonne < this.map[ligne].length; colonne++) {
+        let tile = this.map[ligne][colonne];
+        let definitionCase;
+        if (tile === 1) {
+          definitionCase = this.mur;
+        }
+        if (tile === 0) {
+          definitionCase = this.caseVide;
+        }
+        if (tile === 2) {
+          definitionCase = this.temperature;
+        }
+        this.#dessinerCase(ctx, colonne, ligne, this.tailleGrille, definitionCase);
+      }
+    }
+  }
+  #dessinerCase(ctx, colonne, ligne, tailleGrille, definitionCase) {
+    ctx.drawImage(
+      definitionCase,
+      colonne * this.tailleGrille,
+      ligne * this.tailleGrille,
+      tailleGrille,
+      tailleGrille
+    );
+  }
+
+  setCanvasTaille(canvas) {
+    canvas.width = this.map[0].length * this.tailleGrille;
+    canvas.height = this.map.length * this.tailleGrille;
   }
 }
--- src/Jeu.js
+++ src/Jeu.js
@@ -1,8 +1,15 @@
+import Grille from "./Grille.js";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+const tailleGrille = 40;
+
+const grille = new Grille(tailleGrille);
 
 function gameLoop() {
-  console.log("test");
+  grille.draw(ctx);
 }
+
+grille.setCanvasTaille(canvas);
 
 setInterval(gameLoop, 1000 / 75);
